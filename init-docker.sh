a2a05a74e6e9bc95bdc5f88b4c4af93129fd3798
wanglanwei <dreamcatchwang1991@gmail.com>
2017-02-14 00:11:54 +0800

    update init-docker.sh

--- init-docker.sh
+++ init-docker.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-
-# 从DockerHub Pull下MySQL官方镜像
-# 网速较慢，可以借助shadowsocks+proxychains4翻墙工具下载
 docker pull mysql:latest
-
 docker run --name local-mysql -p 0.0.0.0:3306:3306 -d \
         -e MYSQL_ROOT_PASSWORD='localdev' \
         -e MYSQL_DATABASE='poseidon' mysql
-
-# 查看本地端口
 lsof -i :3306
