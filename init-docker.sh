967a1e9d71eb9eeeff890fd4ba9b3ecc7f9e903d
wanglanwei <wanglanwei@weiche.cn>
2017-02-14 18:15:14 +0800

    update docker

--- init-docker.sh
+++ init-docker.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-docker pull mysql:latest
-docker run --name local-mysql -p 0.0.0.0:3306:3306 -d \
-        -e MYSQL_ROOT_PASSWORD='localdev' \
+
+PREFIX=$(cd `dirname $0`; pwd)
+docker pull mysql:5.7.17
+docker run --name local-mysql-dev -p 0.0.0.0:3306:3306 -d \
+        -v ${PREFIX}:/etc/mysql/conf.d \
+        -e MYSQL_ROOT_PASSWORD='localdevmanager' \
+        -e MYSQL_USER='dev' \
+        -e MYSQL_PASSWORD='jkL8jDequ0IcTTeZ' \
         -e MYSQL_DATABASE='poseidon' mysql
 lsof -i :3306
